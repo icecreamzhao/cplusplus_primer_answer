665502504378dc967378d215cf11e3ecfe9bcd38
zhaotaiqi <1127534615@qq.com>
2022-01-03 20:53:49 +0800

    first commit

---
+++ primerFifth.cpp
@@ -0,0 +1,18 @@
+﻿// primerFifth.cpp: 定义应用程序的入口点。
+//
+
+#include "primerFifth.h"
+
+int main()
+{
+	// 第一章
+	/*Chapter_one one = Chapter_one();
+	one.runAll();*/
+
+	// 第二章
+	/*Chapter_two two = Chapter_two();
+	two.runAll();*/
+
+	// 第三章
+	return 0;
+}
---
+++ primerFifth.h
@@ -0,0 +1,9 @@
+﻿// primerFifth.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include "chapter1/Chapter_1.h"
+#include "chapter2/Chapter_2.h"
+
+// TODO: 在此处引用程序需要的其他标头。
